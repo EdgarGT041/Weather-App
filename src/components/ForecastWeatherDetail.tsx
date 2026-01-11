2e0f204575061299afa867e218c2f89e5bffcbd4
EdgarGT041 <edgarguerrerotirado@gmail.com>
2026-01-11 14:54:34 -0700

    feat: Move ForecastWeatherDetail component to components folder

---
+++ src/components/ForecastWeatherDetail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Container from '@/components/Container';
+type Props = {}
+
+export default function ForecastWeatherDetail({}: Props) {
+  return (
+    <Container>ForecastWeatherDetail</Container>
+  )
+    }
