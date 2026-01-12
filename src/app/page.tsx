13da9ebf7b2a94e2f918a30b3882e284cfbe0f7f
EdgarGT041 <edgarguerrerotirado@gmail.com>
2026-01-11 20:24:10 -0700

    feat: implement location-based weather search and suggestions in Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,9 @@ import { metersToKilometers } from "@/utils/metersToKilometers";
 import WeatherDetails from "@/components/WeatherDetails";
 import { convertWindSpeed } from "@/utils/convertWindSpeed";
 import ForecastWeatherDetail from "@/components/ForecastWeatherDetail";
+import { useAtom } from "jotai";
+import { placeAtom } from "./atom";
+import { useEffect } from "react";
 
 
 // https://api.openweathermap.org/data/2.5/forecast?q=London&appid=49553306a753297d2bec932a77662cf6&cnt=56
@@ -86,15 +89,20 @@ export default function Home() {
 }
 
 function Weather() {
-  const { data, isLoading, error } = useQuery<WeatherData>({
-    queryKey: ['weatherData'],
+  const [place, setPlace] = useAtom(placeAtom);
+  
+  const { data, isLoading, error, refetch } = useQuery<WeatherData>({
+    queryKey: ['weatherData', place],
     queryFn: async () => {
       const { data } = await axios.get<WeatherData>(
-        `https://api.openweathermap.org/data/2.5/forecast?q=London&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
+        `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`
       );
       return data;
     }
   });
+      useEffect(() => {
+    refetch();
+  }, [place, refetch]);
   const firstData = data?.list[0];
 
   console.log('data', data);
@@ -132,7 +140,7 @@ function Weather() {
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-      <Navbar />
+      <Navbar location={data?.city.name} />
       <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
           {/* today data */}
         <section className="space-y-4">
